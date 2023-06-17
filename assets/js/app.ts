47d8c1b7ecf5a7de88046ba167e653ff082b0898
Irina Kats <irina8kats@gmail.com>
2023-06-16 22:31:19 -0400

    fix: linter reports and review points #2-4, 10

--- assets/js/app.ts
+++ assets/js/app.ts
@@ -1,3 +1,2 @@
 import './sprite';
-import './mmenu';
 import './fire_mmenu';
--- assets/js/fire_mmenu.js
+++ assets/js/fire_mmenu.js
@@ -1,16 +1,22 @@
-document.addEventListener(
-        "DOMContentLoaded", () => {
-            const menu = new Mmenu( "#menu", {
-                slidingSubmenus: false
-            }, {
-                    classNames: {
-                        selected: "active"
-                        },
-                    offCanvas: {
-                        page: {
-                            selector: "#page"
-                        }
-                    }
-                });
-        }
-    );
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
+import Mmenu from 'mmenu-js';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const menu = new Mmenu(
+    '#menu',
+    {
+      slidingSubmenus: false,
+    },
+    {
+      classNames: {
+        selected: 'active',
+      },
+      offCanvas: {
+        page: {
+          selector: '#page',
+        },
+      },
+    }
+  ); // eslint-disable-line no-unused-vars
+});
